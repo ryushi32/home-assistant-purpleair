0eac8bf075ca7d69755da991d4c2cb011d9fcb4b
cosmic-driftr <35394281+cosmic-driftr@users.noreply.github.com>
2026-03-10 04:52:11 -0700

    Update for v2.1.8

--- custom_components/purpleair/const.py
+++ custom_components/purpleair/const.py
@@ -57,7 +57,7 @@ SENSORS_MAP = {
     'dewpoint':                {'key': 'current_dewpoint', 'uom': UnitOfTemperature.FAHRENHEIT, 'device_class': SensorDeviceClass.TEMPERATURE, 'icon': 'mdi:water-outline'},
     'pressure':                {'key': 'pressure',         'uom': UnitOfPressure.HPA, 'device_class': SensorDeviceClass.PRESSURE, 'icon': 'mdi:gauge'},
     'rssi':                    {'key': 'rssi',             'uom': 'dBm', 'device_class': SensorDeviceClass.SIGNAL_STRENGTH, 'icon': 'mdi:wifi'}
-    'voc_experimental':        {'key': 'gas_680',          'uom': None, 'device_class': None, 'icon': 'mdi:flask-outline'},
+    'voc_iaq_index':           {'key': 'gas_680',          'uom': None, 'device_class': None, 'icon': 'mdi:flask-outline'},
     'voc_iaq_class':           {'key': 'voc_iaq_class',    'uom': None, 'device_class': None, 'icon': 'mdi:home-air-filter'},
 }
 
