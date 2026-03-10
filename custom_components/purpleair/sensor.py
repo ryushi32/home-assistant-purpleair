3df8a7075bbc125218772d1aebe04b01a1819b4b
cosmic-driftr <35394281+cosmic-driftr@users.noreply.github.com>
2026-03-09 22:52:52 -0700

    Clean up code

--- custom_components/purpleair/sensor.py
+++ custom_components/purpleair/sensor.py
@@ -4,24 +4,15 @@ import logging
 from homeassistant.helpers.dispatcher import async_dispatcher_connect
 from homeassistant.components.sensor import SensorEntity
 
-from .const import DISPATCHER_PURPLE_AIR, DOMAIN, MANUFACTURER, SENSORS_MAP, SENSORS_DUAL_ONLY, MODEL_PA_FLEX, MODEL_PA_2
+from .const import DISPATCHER_PURPLE_AIR, DOMAIN, MANUFACTURER, SENSORS_MAP
 
 _LOGGER = logging.getLogger(__name__)
 
 
 async def async_setup_entry(hass, config_entry, async_schedule_add_entities):
-    _LOGGER.debug('Registering aqi sensor with data: %s', config_entry.data)
-
-    # Backwards compat for sensors added before 'is_dual' key created in config_flow
-    if 'is_dual' in config_entry.data:
-        is_dual = config_entry.data['is_dual']
-    else:
-        is_dual = config_entry.data['model'] in [MODEL_PA_FLEX, MODEL_PA_2]  # Backup to test for dual sensors
-
     entities = []
     for index, entity_desc in SENSORS_MAP.items():
-        if is_dual or entity_desc['key'] not in SENSORS_DUAL_ONLY:
-            entities.append(PurpleAirQualitySensor(hass, index, config_entry, entity_desc))
+        entities.append(PurpleAirQualitySensor(hass, index, config_entry, entity_desc))
 
     async_schedule_add_entities(entities)
 
